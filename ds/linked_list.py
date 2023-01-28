9a0fcb9d5263da110894cd7b5aa62c9151221a9c
deekshithtirumala <105743252+deekshithtirumala@users.noreply.github.com>
2023-01-28 22:54:22 +0530

    Update linked_list.py

--- ds/linked_list.py
+++ ds/linked_list.py
@@ -1,11 +1,10 @@
 class Node:
-    def __init__(self,val=0, left=None, right=None):
+    def __init__(self,val, next_=None):
         self.val = val
-        self.left = left
-        self.right = right
+        self.next = next_
 
 class LinkedList:
-    def __init__(self, data=0):
+    def __init__(self, data):
         self.head = Node(data)
     def pop(self):
         temp = self.head
@@ -14,4 +13,38 @@ class LinkedList:
         else:
             raise ('no node present')
         return temp
+    def add(self, data):
+        temp = self.head
+        if self.head==None:
+            self.head = Node(data)
+            return
+        while temp.next!=None:
+            temp = temp.next
+        temp.next = Node(data)
     
+    def size(self):
+        count = 0
+        temp = self.head
+        while temp!=None:
+            count+=1
+            temp = temp.next
+        return count
+    def isEmpty(self):
+        if self.head==None:
+            return True
+        return False
+    def radd(self, data):
+        #add the data before the head
+        temp = Node(data)
+        temp.next = self.head
+        self.head = temp
+    def list(self):
+        #returns list of linkedlist
+        if self.head==None:
+            return []
+        lst =[]
+        temp = self.head
+        while temp!=None:
+            lst.append(temp.val)
+            temp = temp.next
+        return lst
