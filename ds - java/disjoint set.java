7ad985ea189d1ed1a0d0b86aab227228837424f9
deekshithtirumala <105743252+deekshithtirumala@users.noreply.github.com>
2023-02-13 21:35:29 +0530

    Update disjoint set.java

--- ds - java/disjoint set.java
+++ ds - java/disjoint set.java
@@ -1,45 +1,51 @@
 /******************************************************************************
 
 *******************************************************************************/
-import java.util.*;
 class DisjointSet{
-    List<Integer> parent = new ArrayList<Integer>();
     List<Integer> rank = new ArrayList<Integer>();
+    List<Integer> parent = new ArrayList<Integer>();
     public DisjointSet(int n){
-        for(int i=0;i<n;i++){
+        for(int i=0;i<=n;i++){
             rank.add(0);
             parent.add(i);
         }
     }
-    public int findUlParent(int u){
-        //base case :
-        if (u==parent.get(u)){
+    public int findUlparent(int u){
+        if(u==parent.get(u)){
             return u;
         }
         
-        //recurrsive case :
-        parent.set(u, findUlParent(parent.get(u)));
+        //recurrsive case
+        parent.set(u, findUlparent(parent.get(u)));
         return parent.get(u);
     }
+    
     public void unionSet(int u, int v){
-        //if ultimate parent not equal to target ultimate
-        int ulpar_u = findUlParent(u);
-        int ulpar_v = findUlParent(v);
+        int ultpar_u = findUlparent(u);
+        int ultpar_v = findUlparent(v);
         
-        if (ulpar_v==ulpar_u) return;
-        
-        else if(rank.get(ulpar_u)<rank.get(ulpar_v)){
-            parent.set(u, ulpar_v);
+        if(ultpar_u==ultpar_v){
+            return ;
         }
-        else if(rank.get(ulpar_v)<rank.get(ulpar_u)){
-            parent.set(v, ulpar_u);
+        
+        else if(rank.get(ultpar_u)<rank.get(ultpar_v)){
+            //adding u to v --> u's parent = v 
+            //1      4        4
+            //|\     |\      /|\
+            //2 3    5 6    1 5 6
+            //       | |   /| | |
+            //       8 7  2 3 8 7
+            //rank of 1's = 1 and rank of 4's = 2
+            //ultimate parent of 3's parent is set to ultimate parent 7
+            parent.set(ultpar_u, ultpar_v);
+        }
+        else if(rank.get(ultpar_u)> rank.get(ultpar_v)){
+            parent.set(ultpar_v, ultpar_u);
         }
         else{
-            //ranks are equal then chaning v's parent as ulpar_u and rank+=1
-            int rankU = rank.get(u);
-            rank.set(u, rankU+1);
-            parent.set(v, ulpar_u);
-            
+            //rank are equal rank should be incremented
+            parent.set(ultpar_v, ultpar_u);
+            rank.set(ultpar_u, rank.get(ultpar_u)+1);
         }
     }
 }
